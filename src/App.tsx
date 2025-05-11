ca957fbf21fcba7f98159475786d8d4c6ba456a8
Krzysztof Piatkowski <krzysztofpiatkowski1987@gmail.com>
2025-05-11 18:24:06 +0200

    Task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+export class App extends React.Component<{}, { pressedKey: string | null }> {
+  state = {
+    pressedKey: null,
+  };
+
+  handleKeyUp = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    this.setState({ pressedKey: event.key });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <input
+          className="App__input"
+          onKeyUp={this.handleKeyUp}
+          autoFocus
+          style={{ opacity: 0, position: 'absolute' }}
+        />
+
+        {this.state.pressedKey === null ? (
+          <p className="App__message">Nothing was pressed yet</p>
+        ) : (
+          <p className="App__message">
+            The last pressed key is [{this.state.pressedKey}]
+          </p>
+        )}
+      </div>
+    );
+  }
+}
